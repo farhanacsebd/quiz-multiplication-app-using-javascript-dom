23d4055e6f705aa626454eadd4f616bcd32f398e
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-27 02:10:11 +0600

    catch the question or input ans

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
+const question = document.getElementById('question');
+const userInput = document.getElementById('input');
+let score = document.getElementById('score');
+
+
 document.getElementById('submit').addEventListener('click',function(){
-    num1 = Math.random()*10
-    console.log(num1);
+    const num1 = Math.round(Math.random()*10);
+    const num2 = Math.round(Math.random()*10);
+
+    
+    question.innerText = `What is ${num1} multiply by ${num2}?`
+    
+    const correctAnus = num1 * num2;
+
+    const userAns = Number(userInput.value);
+     
+    if(userAns === correctAnus){
+        score++;
+    }
+    else{
+        score--;
+    }
+
+
+
 })
