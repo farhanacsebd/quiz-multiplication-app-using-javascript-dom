c0a26774ac6e7af6c5ec4ff81bfaef72ee754786
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-27 21:51:27 +0600

    code rearrange

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
+const question = document.getElementById('question');
+const userInput = document.getElementById('input');
+const formE1 = document.getElementById('form');
+let scoreE1 = document.getElementById('score');
+
+
+
 // random calculation start from here 
 
 const num1 = Math.round(Math.random()*10);
 const num2 = Math.round(Math.random()*10);
-   
-
-const question = document.getElementById('question');
-
-
-const userInput = document.getElementById('input');
-
-
-const formE1 = document.getElementById('form');
-
-
-let scoreE1 = document.getElementById('score');
 
 
 let score = JSON.parse(localStorage.getItem("score"));
@@ -32,7 +27,7 @@ const correctAnus = num1 * num2;
 formE1.addEventListener('submit',function(){
 
     const userAns = +userInput.value;     // ( + ) means typeof number
-     
+
     if(userAns === correctAnus){
         score++;
         updateLocalStorage()     //call the function from localstorage
