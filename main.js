13181c8ab4398c92cee142e76e3ad4f317c98d17
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-27 21:21:14 +0600

    try to catch localstorage

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
+const num1 = Math.round(Math.random()*10);
+const num2 = Math.round(Math.random()*10);
+   
+
 const question = document.getElementById('question');
+
+
 const userInput = document.getElementById('input');
-let score = document.getElementById('score');
 
 
-document.getElementById('submit').addEventListener('click',function(){
-    const num1 = Math.round(Math.random()*10);
-    const num2 = Math.round(Math.random()*10);
+const formE1 = document.getElementById('form')
 
-    
-    question.innerText = `What is ${num1} multiply by ${num2}?`
-    
-    const correctAnus = num1 * num2;
 
-    const userAns = Number(userInput.value);
+let scoreE1 = document.getElementById('score');
+
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+
+if(!score){
+    score = 0;
+}
+scoreE1.innerText = `score: ${score}`
+
+question.innerText = `What is ${num1} multiply by ${num2}?`
+
+const correctAnus = num1 * num2;
+
+formE1.addEventListener('submit',function(){
+
+    const userAns = +userInput.value;     // ( + ) means typeof number
      
     if(userAns === correctAnus){
         score++;
+        updateLocalStorage()     //call the function from localstorage
     }
     else{
         score--;
+        updateLocalStorage()    //call the function from localstorage
     }
 
-
-
 })
+
