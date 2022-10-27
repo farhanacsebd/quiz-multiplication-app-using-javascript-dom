9e5a7f3734e3e5c09f91bb935f388736723718cc
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-27 21:42:43 +0600

    local storage added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// random calculation start from here 
+
 const num1 = Math.round(Math.random()*10);
 const num2 = Math.round(Math.random()*10);
    
@@ -8,7 +10,7 @@ const question = document.getElementById('question');
 const userInput = document.getElementById('input');
 
 
-const formE1 = document.getElementById('form')
+const formE1 = document.getElementById('form');
 
 
 let scoreE1 = document.getElementById('score');
@@ -20,9 +22,10 @@ let score = JSON.parse(localStorage.getItem("score"));
 if(!score){
     score = 0;
 }
-scoreE1.innerText = `score: ${score}`
 
-question.innerText = `What is ${num1} multiply by ${num2}?`
+scoreE1.innerText = `score: ${score}`;
+
+question.innerText = `What is ${num1} multiply by ${num2}?`;
 
 const correctAnus = num1 * num2;
 
@@ -39,5 +42,11 @@ formE1.addEventListener('submit',function(){
         updateLocalStorage()    //call the function from localstorage
     }
 
-})
+});
 
+
+
+// localstorage
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score))
+}
